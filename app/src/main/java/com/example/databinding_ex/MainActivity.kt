468361b211f9977fbca3e4c69273700cf654d582
bsh6463 <bsh9510@gmail.com>
2022-03-17 21:04:31 +0900

    Initial commit

---
+++ app/src/main/java/com/example/databinding_ex/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.databinding_ex
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.databinding_ex.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+    /*    //databinding 미사용
+        val btn = findViewById<Button>(R.id.testBtnId)
+        btn.setOnClickListener {
+            Toast.makeText(this, "clicked", Toast.LENGTH_LONG).show()
+        }
+    */
+        //DataBinding
+        binding.testBtnId.setOnClickListener {
+            Toast.makeText(this, "clicked", Toast.LENGTH_LONG).show()
+        }
+    }
+}
